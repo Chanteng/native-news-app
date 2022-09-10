f5fe30d0ffc9613a0763ae168b309d89e5bae821
Chanteng <yrnchanteng@gmail.com>
2022-09-10 09:17:38 +0000

    feching api

--- Component/Article.js
+++ Component/Article.js
@@ -1,46 +1,92 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Article = () => {
   return (
-   <SafeAreaView style={styles.container}>
-    <Image source={{
-        uri: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-    }} 
-    style={styles.img}
-    />
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        }}
+        style={styles.img}
+      />
 
-    {/* title */}
-    <Text style={styles.title}>News App </Text>
+      <View style={{ padding: 20 }}>
+        {/* title */}
+        <Text style={styles.title}>News App </Text>
 
- {/* description */}
- <Text style={styles.text}>Watch Out </Text>
+        {/* description */}
+        <Text style={styles.text}>Watch Out </Text>
 
-   </SafeAreaView>
-  )
-}
+        {/* data */}
+        <View style={styles.data}>
+          <Text style={styles.heading}>
+            By: <Text style={styles.author}>OIS</Text>{" "}
+          </Text>
+          <Text style={styles.date}>10th Sept. 22</Text>
+        </View>
 
-export default Article
+        {/* source */}
+        <View style={{ marginTop: 10 }}>
+          <Text style={styles.source}>Programming</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Article;
 
 const styles = StyleSheet.create({
-    container: {
-        width: "98%",
-        alignSelf: "center"
+  container: {
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 40,
+    shadowOpacity: 0.5,
+    shadowColor: "#000",
+    shadowOffset: {
+      height: 5,
+      width: 5,
     },
+    backgroundColor: "#fff",
+    marginTop: 20,
+  },
 
-    img: {
-        height: 200,
-        width: "100%"
-    },
+  img: {
+    height: 200,
+    width: "100%",
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+  },
 
-    title: {
-        fontSize: 18,
-        fontWeight: "600",
-        marginTop: 10,
-    },
-    text: {
-        fontSize: 16,
-        fontWeight: "400",
-        marginTop: 10,
-    }
-})
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: "400",
+    marginTop: 10,
+  },
+  data: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  heading: {},
+  author: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  date: {
+    fontWeight: "bold",
+    color: "blue",
+    fontSize: 15,
+  },
+  source: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,15 +1,35 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
-import Article from '../Component/Article'
+import { StyleSheet, Text, View, Scroll, ScrollView } from "react-native";
+import React, {useState} from "react";
+import Article from "../Component/Article";
 
 const HomeScreen = () => {
+
+    const [article, setArticle] = useState([]);
+
+  const getNews = () => {
+    // Make a request for a user with a given ID
+    axios
+      .get("/user?ID=12345")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
   return (
-   <SafeAreaView style={styles.container}>
-    <Article />
-   </SafeAreaView>
-  )
-}
+    <ScrollView style={styles.container}>
+      <Article />
+    </ScrollView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
